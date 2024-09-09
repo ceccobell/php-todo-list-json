4a53930e9483ee4cdc2618cb2f28911ee873a927
ceccobell <161962926+ceccobell@users.noreply.github.com>
2024-09-09 21:06:42 +0200

    [UPD] created file server.php to read file json

--- index.php
+++ index.php
@@ -3,13 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://unpkg.com/vue@next"></script>
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
     <title>PHP ToDoList</title>
 </head>
 <body>
     <div id="app">
-        
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h1>ToDo List</h1>
+                </div>
+            </div>
+        </div>
     </div>
     <script src="./js/script.js"></script>
 </body>
---
+++ server.php
@@ -0,0 +1,4 @@
+<?php
+    $string = file_get_contents('todo-list.json');
+    $todoList = json_decode($string);
+?>
